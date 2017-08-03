f3fee5371fc0073c32f96b978c0b4940c1beb07d
Nathaniel Saul <nat@saulgill.com>
2017-08-03 00:36:12 -0700

    removing duplicate files and other superfluous data

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# setup virtual environment - if `python` does not point to python3, then you'll have to use python3.
+#   Also, if you use anaconda, you'll have to follow different steps for the venv
 python -m venv venv
 source venv/bin/activate
+
+
 pip install -r requirements.txt
+
+# Download spectra data from Google Drive
 python src/accessSpectra/loadFromDrive.py
+
+# Unzip the spectra data
 tar -xvzf data/raw_data/blackhole_spectra.tar.gz -C data/raw_data
+
+
